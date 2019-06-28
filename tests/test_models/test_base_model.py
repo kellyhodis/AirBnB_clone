d80c3e4dd773a0e8c7c32218bda33525fee8989d
jmtes <juliennetesoro@gmail.com>
2019-06-28 22:07:00 +0000

    Add test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,55 +6,65 @@ import unittest
 from models.base_model import BaseModel
 from io import StringIO
 import sys
+import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     ''' Test cases for the class BaseModel. '''
     def setUp(self):
         ''' Set up each test case. '''
-        b = BaseModel()
+        self.b = BaseModel()
 
     def tearDown(self):
         ''' Clean up after each test case. '''
-        del b
+        del self.b
+
+    def test_instance_type(self):
+        ''' Test if b is of type BaseModel. '''
+        assert type(self.b) == BaseModel
 
     def test_id_is_str(self):
         ''' Make sure id attr is a str. '''
-        assert type(b.id) == str
+        assert type(self.b.id) == str
 
     def test_created_at(self):
         ''' Validate created_at attr. '''
-        assert type(b.created_at) == datetime.datetime
+        assert type(self.b.created_at) == datetime.datetime
 
     def test_updated_at(self):
         ''' Validate updated_at attr. '''
-        assert type(b.updated_at) == datetime.datetime
+        assert type(self.b.updated_at) == datetime.datetime
+
+    def test_created_updated_same(self):
+        ''' Test whether or not created_at and updated_at are the same. '''
+        assert self.b.created_at == self.b.updated_at
+
+    def test_different_ids(self):
+        ''' Test whether or not two instances have different IDs. '''
+        c = BaseModel()
+        assert self.b.id != c.id
 
     def test_str(self):
         ''' Test __str__ method avg use case. '''
-        captured_output = StringIO()
-        sys.stdout = captured_output
-        print(b)
-        sys.stdout = sys.__stdout__
-        captured_output = captured_output.getvalue()
-        expected = '[BaseModel] ({:s}) {}'.format(b.id, b.__dict__)
-        assert captured_output == expected
+        str_return = self.b.__str__()
+        assert type(str_return) == str
 
     def test_save(self):
         ''' Test save method avg use case. '''
-        old_update_time = b.updated_at
-        b.save()
-        assert b.updated_at != old_update_time
+        old_update_time = self.b.updated_at
+        self.b.save()
+        assert self.b.updated_at != old_update_time
 
     def test_save_args(self):
         ''' Test save method with arg provided. '''
         with self.assertRaises(TypeError) as context:
-            b.save('time n place')
+            self.b.save('time n place')
         self.assertTrue('2 were given' in str(context.exception))
 
     def test_to_dict(self):
         ''' Test to_dict avg use case. '''
-        b_dict = b.to_dict()
+        b_dict = self.b.to_dict()
+        assert type(b_dict) == dict
         assert 'id' in b_dict
         assert type(b_dict['id']) == str
         assert 'created_at' in b_dict
@@ -67,5 +77,5 @@ class TestBaseModel(unittest.TestCase):
     def test_to_dict_args(self):
         ''' Test to_dict method with arg provided. '''
         with self.assertRaises(TypeError) as context:
-            b.to_dict('mist')
+            self.b.to_dict('mist')
         self.assertTrue('2 were given' in str(context.exception))
