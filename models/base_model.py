84973fc3f97e38ff401b797e185092e5d488ed20
jmtes <juliennetesoro@gmail.com>
2019-06-28 22:21:43 +0000

    Change updated_at assignment in init

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel():
         ''' Initialize instance of BaseModel. '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         ''' Return string representation of BaseModel instance. '''
