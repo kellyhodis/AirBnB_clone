9be00bcb5a59d19225e288b0f2d6a6fc5c4ae8e1
Kelly <kelly.hodis@protonmail.com>
2019-06-30 00:42:16 +0000

    Added error checks

---
+++ console.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+''' This module contains the entry point of the command interpreter.
+'''
+import cmd
+import models
+import inspect
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class HBNBCommand(cmd.Cmd):
+    ''' Define commands for HBNB CLI. '''
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        ''' Quit command to exit the program. '''
+        return True
+
+    def do_EOF(self, line):
+        ''' Exit the program. '''
+        return True
+
+    def emptyline(self):
+        ''' Do nothing if empty line is entered. '''
+        pass
+
+    def do_create(self, line):
+        ''' Create new instance of a class, save it, and print its id.
+
+            Args:
+                line - name of class to make new instance of.
+        '''
+        new_ins = eval(line)()
+        models.storage.new(new_ins)
+        models.storage.save()
+        print(new_ins.id)
+
+    def do_show(self, line):
+        ''' Print string repr of instance. '''
+        if line == '':
+            print("** class name missing **")
+        else:
+            args = line.split(' ')
+            try:
+                var = eval(args[0])()
+            except:
+                print("** class doesn't exist **")
+                return
+            try:
+                args[1]
+            except:
+                print("** instance id missing **")
+                return
+            try:
+                models.storage._FileStorage__objects[args[0] + '.' + args[1]]
+            except:
+                print("** no instance found **")
+                return
+            else:
+                if (args[0] + '.' + args[1]) in models.storage._FileStorage__objects:
+                    print(models.storage._FileStorage__objects[args[0] + '.' + args[1]])
+
+    def do_destroy(self, line):
+        ''' Delete instance of a given class. '''
+        args = line.split(' ')
+        del models.storage._FileStorage__objects[args[0] + '.' + args[1]]
+        models.storage.save()
+
+    def do_all(self, class_name=''):
+        ''' Print all string representations of all instances.
+
+            Args:
+                class_name - Optional class.
+        '''
+        lst = []
+        for obj in models.storage._FileStorage__objects.values():
+            if class_name:
+                try:
+                    var = eval(args[0])()
+                except:
+                    print("** class doesn't exist **")
+                    return
+                if class_name == obj.__class__.__name__:
+                    lst.append(str(obj))
+            else:
+                lst.append(str(obj))
+        print(lst)
+
+    def do_update(self, line):
+        ''' Update instance attribute and save changes to JSON file. '''
+        if line == '':
+            print("** class name missing **")
+        else:
+            args = line.split(' ')
+            try:
+                var = eval(args[0])()
+            except:
+                print("** class doesn't exist **")
+                return
+            try:
+                args[1]
+            except:
+                print("** instance id missing **")
+                return
+            try:
+                models.storage._FileStorage__objects[args[0] + '.' + args[1]]
+            except:
+                print("** no instance found **")
+                return
+            try:
+                args[2]
+            except:
+                print("** attribute name missing **")
+                return
+            try:
+                args[3]
+            except:
+                print("** value missing **")
+                return
+            args[3] = args[3][1:-1]
+            if args[0] + '.' + args[1] in models.storage._FileStorage__objects:
+                obj = models.storage._FileStorage__objects[args[0] + '.' + args[1]]
+                setattr(obj, args[2], args[3])
+                models.storage.save()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
