257bff700acf6c0dfc16400c1702b48eade23c6c
kellyhodis <668@holbertonschool.com>
2019-07-01 15:04:43 -0700

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' This module defines the Amenity class.
+    Attributes:
+        Amenity - Subclass of BaseModel.
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    ''' Represent the amenities of an accommodation. '''
+    name = ''
